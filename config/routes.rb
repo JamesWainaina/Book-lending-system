e9bea9504f2c442a65d3d6bf433f46000ebccf92
JamesWainaina <jamesgatheru9905@gmail.com>
2025-02-13 10:01:32 +0300

    fix layout Register page and login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # User authentication routes (Devise or default Rails auth)
   devise_for :users
+
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+ end
   
   # Static home page
   root to: "home#index"
